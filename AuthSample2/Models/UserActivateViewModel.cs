90e0334038fbb0aa9c6e847d21224559979fe4eb
mic <mic>
2016-10-03 04:13:26 +0900

    enable lockout and activate

---
+++ AuthSample2/Models/UserActivateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthSample2.Models
+{
+    public class UserActivateViewModel
+    {
+        public string UserId { get; set;  }
+
+        public string Password { get; set; }
+
+        public string PasswordConfirmation { get; set; }
+    }
+}
